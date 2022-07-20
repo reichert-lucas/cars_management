0248afe1db663462052a0b8aa3fd1651e45ab606
Lucas Coelho Reichert <lucas3d4783@gmail.com>
2022-07-20 02:23:39 -0300

    Criação de arquivos para latouts default e de login

---
+++ front/layouts/auth.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <nuxt />
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'auth-layout',
+
+    created () {
+        this.checktoken()
+    },
+
+    methods: {
+        ...mapActions('login', ['checktoken'])
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .auth__layout {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .position_theme_icon {
+            position: absolute;
+            right: 10rem;
+            top: 2rem;
+        }
+
+        .form__validate__style {
+            max-width: 30rem;
+            width: 25rem;
+        }
+    }
+</style>
---
+++ front/layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'DefaultLayout',
+
+  created () {
+    this.checktoken()
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+    ...mapActions('login', ['checktoken', 'signOut']),
+
+    sair(){
+      this.signOut()
+        .then((res) => {
+            this.$toast.success('Logout realizado com sucesso.')
+            this.$router.push({path: '/'})
+          })
+          .catch((error) => {
+            this.$toast.error(error.message)
+          })
+    },
+
+    closeMenu(){
+      this.drawer = false
+      this.miniVariant = false
+    }
+  },
+
+  computed: {
+    ...mapState('loader', ['loading']),
+    ...mapState('login', ['user'])
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
