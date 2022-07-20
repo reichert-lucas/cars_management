79d901febf56748cfbdaaf806675f83d93547f4e
Lucas Coelho Reichert <lucas3d4783@gmail.com>
2022-07-20 04:32:02 -0300

    Refatoração do middleware

--- front/middleware/auth.js
+++ front/middleware/auth.js
@@ -1,13 +1,15 @@
-export default function ({route, redirect}){
-    const token = localStorage.getItem('user-token')
+export default function ({ route, redirect }) {
+  const token = localStorage.getItem('user-token')
 
-    if (route.path !== '/') { //painel
-        if(!token) {
-            return redirect('/')
-        }
-    } else if (route.path === '/') { //login
-        if(token) {
-            return redirect('/dashboard')
-        }
+  if (route.path !== '/') {
+    // dashboard
+    if (!token) {
+      return redirect('/')
     }
+  } else if (route.path === '/') {
+    // login
+    if (token) {
+      return redirect('/dashboard')
+    }
+  }
 }
